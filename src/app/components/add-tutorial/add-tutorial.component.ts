04aa66d55e426129845f6a3c23dd8f20d3a86b43
davidauza.engineer <davidauza01@gmail.com>
2020-05-28 21:10:18 -0500

    Implement add tutorial component

--- src/app/components/add-tutorial/add-tutorial.component.ts
+++ src/app/components/add-tutorial/add-tutorial.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TutorialService } from '../../services/tutorial.service';
 
 @Component({
   selector: 'app-add-tutorial',
@@ -6,10 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-tutorial.component.css']
 })
 export class AddTutorialComponent implements OnInit {
+  tutorial = {
+    title: '',
+    description: '',
+    published_status: 'draft'
+  };
+  submitted = false;
 
-  constructor() { }
+  constructor(private tutorialService: TutorialService) { }
 
   ngOnInit() {
   }
 
+  saveTutorial() {
+    const data = {
+      title: this.tutorial.title,
+      description: this.tutorial.description
+    };
+
+    this.tutorialService.create(data)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.submitted = true;
+        },
+        error => {
+          console.log(error);
+        }
+      );
+  }
+
+  newTutorial() {
+    this.submitted = false;
+    this.tutorial = {
+      title: '',
+      description: '',
+      published_status: 'draft'
+    };
+  }
 }
